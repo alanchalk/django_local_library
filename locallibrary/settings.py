992f0e52e310c0c92a1b19911391ef1fa2e7819c
alanchalk <alanchalk@gmail.com>
2023-09-13 19:24:50 +0100

    Change ALLOWED_HOSTS to *

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)#%_bxm2@qm$lg_=*^z1qr0lr9$6jo)+o0r%p9b7yu^fhw)_t*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
